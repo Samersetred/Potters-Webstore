67cbf971d7fd54f1c643f6fd05718c784a91d750
Somerset Dromgoole <samersetredram@gmail.com>
2025-01-23 16:37:10 +0000

    added annotated OrderItem class

--- potterystore/models.py
+++ potterystore/models.py
@@ -24,6 +24,28 @@ class Product(models.Model):
 # the price can be stored as floating-point number in case the product has a decimal point. I justify this as good practise although I don't intend to sell pottery for £45.72
 # Once again it returns a string representation of the name of the product for database purposes
 
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
 
+    def __str__(self):
+        return str(self.id)
+# customer is a ForeignKey field. This field creates a link between the parent model (Customer) and the child model (Order). This lets me track multiples instances of the child Order model to one parent Customer model. So one customer can now place multiple orders, all of which are tracked. 
+# Also, if the referenced parent 'Customer' model is deleted, the 'customer' field is deleted
 
-    
+# date ordered records the date and time of the order being placed, set to the current date and time, which cannot be changed
+# complete indicates whether an order has been completed
+# transaction_id creates a field to store a unique transaction ID for the order, this is optional
+# the str method returns the id to the admin database as a string representation
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+# Here we're creating a class for our ordered product, linking it to the Product and Order classes, noting the quantity and date the product was added to the order
+# This will store information in our database that will be used by our cart page.
+
