414dd0454916593459ae0e9f1760f82698eecd67
Somerset Dromgoole <samersetredram@gmail.com>
2025-01-28 03:13:15 +0000

    added the necessary login and register pages to create a user

--- potterystore/urls.py
+++ potterystore/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from .views import store
 from .views import cart
 from .views import checkout
+from .views import register
+from .views import login_view
 
 urlpatterns = [
 #    path("", home_page_view),
     path("", store, name="store"),
     path("cart/", cart, name="cart"),
     path("checkout/", checkout, name="checkout"),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
 ]
--- potterystore/views.py
+++ potterystore/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+from .models import Customer
 # import * imports all of my models, instead of specifying them one by one
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Customer.objects.create(user=user, email=form.cleaned_data.get('email'))
+            login(request, user)
+            return redirect('store')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'potterystore/register.html', {'form': form})
 
-# Create your views here.
-
-# def home_page_view(request):
-#    return HttpResponse('Hello, Worldies')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('store')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'potterystore/login.html', {'form': form})
 
 def store(request):
     products = Product.objects.all()
@@ -15,14 +40,19 @@ def store(request):
 # This 
 
 def cart(request):
-
     if request.user.is_authenticated:
-        customer = request.user.customer
+        try:
+            customer = request.user.customer
+        except Customer.DoesNotExist:
+            customer = Customer.objects.create(user=request.user)
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
-        #empty cart
         items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'potterystore/cart.html', context)
 
     # This is for the cart, and our cart page. If the user has made an account (is authenticated), and added items to their cart, but not checked out, this maintains the items in their cart upon their next visit to the site
     # Here I'm retrieving or creating an order for the authenticated user, retrieving all items associated with that order (anything that's been put into the cart)
@@ -33,8 +63,6 @@ def cart(request):
     
     # if the user is just an unlogged in guest, their items are just an empty list, nothing in their cart, cart displays a zero
 
-    context = {'items':items}
-    return render(request, 'potterystore/cart.html', context)
 
 def checkout(request):
     context = {}
